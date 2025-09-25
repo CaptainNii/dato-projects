381b486faa7222df7d2ab34ead551c69c4855fde
CaptainNii <niiblazer@gmail.com>
2025-09-25 12:15:54 +0000

    create flyway_static_migration.sh

---
+++ docker-projects/rentzone/flyway_migrate-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+S3_URI=
+RDS_ENDPOINT=
+RDS_DB_NAME=
+RDS_DB_USERNAME=
+RDS_DB_PASSWORD=
+
+# Update all packages
+sudo yum update -y
+
+# Download and extract Flyway
+sudo wget -qO- https://download.red-gate.com/maven/release/com/redgate/flyway/flyway-commandline/10.9.1/flyway-commandline-10.9.1-linux-x64.tar.gz | tar -xvz 
+
+# Create a symbolic link to make Flyway accessible globally
+sudo ln -s $(pwd)/flyway-10.9.1/flyway /usr/local/bin
+
+# Create the SQL directory for migrations
+sudo mkdir sql
+
+# Download the migration SQL script from AWS S3
+sudo aws s3 cp "$S3_URI" sql/
+
+# Run Flyway migration
+flyway -url=jdbc:mysql://"$RDS_ENDPOINT":3306/"$RDS_DB_NAME" \
+  -user="$RDS_DB_USERNAME" \
+  -password="$RDS_DB_PASSWORD" \
+  -locations=filesystem:sql \
+  migrate
